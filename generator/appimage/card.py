173aabc66c3a1d0a4241f82850026f737a290817
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-08 00:10:58 +0300

    Add Card helper class for formatting

---
+++ generator/appimage/card.py
@@ -0,0 +1,29 @@
+class Card:
+    def __init__(self, app):
+        self.app = app
+
+        self.source_code_url = ''
+        self.is_github = ''
+        self.left_description = ''
+        self.right_description = 'Download'
+
+        # if the appimage is hosted from GitHub
+        if app['github'] is not None:
+            self.set_github_specific_attr()
+
+    def set_github_specific_attr(self):
+        self.source_code_url = \
+            "https://github.com/{}".format(self.app['github'][0].get('url'))
+        self.is_github = 'github'
+        self.left_description = 'Github'
+
+    def __getitem__(self, item):
+        return self.app[item]
+
+    def __getattr__(self, item):
+        return self.app[item]
+
+    @property
+    def name_lower_case(self):
+        return self.app['name'].lower()
+
